890e4e8a28e58839384420d7e0b429264d2559d6
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-08-26 16:06:05 -0400

    learn: instance of variables

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Hello;
+
+Console.WriteLine("Hello, World!");
 
 // C# most of the time run the code in line sequence, but there are some exceptions. For example, when you use async/await, the code may not run in a strict sequential order.
 
@@ -18,3 +20,7 @@ void DoSomething()
 const double pi = 3.14159; // This is a constant value that cannot be changed.
 
 Console.WriteLine(pi); // Output: 3.14159
+
+// Run code from Variables.cs
+Variables variables = new Variables();
+variables.Run();
---
+++ Hello/Variables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello
+{
+    public class Variables
+    {
+        public void Run()
+        {
+            // Variable declaration and initialization
+            int age = 25; // Integer variable
+            string name = "John"; // String variable
+            bool isStudent = true; // Boolean variable
+            // Output the values of the variables
+            Console.WriteLine($"Name: {name}, Age: {age}, Is Student: {isStudent}");
+        }
+    }
+}
