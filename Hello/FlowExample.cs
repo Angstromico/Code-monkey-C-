f522d4d389919c95e00606da7809f2226d4abbd8
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-08-27 09:47:04 -0400

    refactor: flow example

---
+++ Hello/FlowExample.cs
@@ -0,0 +1,20 @@
+﻿namespace Hello
+{
+    internal class FlowExample
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Start");
+
+            DoSomething();
+
+            Console.WriteLine("End");
+        }
+
+        private static void DoSomething()
+        {
+            Console.WriteLine("Inside");
+        }
+    }
+}
+``
--- Hello/Program.cs
+++ Hello/Program.cs
@@ -4,16 +4,7 @@ Function.Grettings();
 
 // C# most of the time run the code in line sequence, but there are some exceptions. For example, when you use async/await, the code may not run in a strict sequential order.
 
-Console.WriteLine("Start");
-
-DoSomething();
-
-Console.WriteLine("End");
-
-void DoSomething()
-{
-    Console.WriteLine("Inside");
-}
+FlowExample.Run();
 
 // The DoSomething method is written after the call, but execution jumps there and then comes back.
 
