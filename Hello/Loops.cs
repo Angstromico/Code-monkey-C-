adc50b7c24c9f55a87a320aef13dab3b53ace465
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-08-31 09:38:57 -0400

    learn: loops

---
+++ Hello/Loops.cs
@@ -0,0 +1,38 @@
+﻿namespace Hello
+{
+    internal class Loops
+    {
+        public static void Run()
+        {
+            // For loop
+            Console.WriteLine("For loop:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // While loop
+            Console.WriteLine("While loop:");
+            int j = 0;
+            while (j < 5)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            // Do-while loop
+            Console.WriteLine("Do-while loop:");
+            int k = 0;
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+            } while (k < 5);
+            // Foreach loop
+            Console.WriteLine("Foreach loop:");
+            string[] fruits = { "Apple", "Banana", "Cherry" };
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
--- Hello/Program.cs
+++ Hello/Program.cs
@@ -32,3 +32,5 @@ bool result2 = isBool(-3);
 Console.WriteLine($"Result 1: {result1}, Result 2: {result2}");
 
 Arrs.Run();
+
+Loops.Run();
