cfe4777fb3bab8e9b78c46e52eede1741e2f2d89
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-08-31 11:37:38 -0400

    learn: update class value

--- Hello/Classes.cs
+++ Hello/Classes.cs
@@ -6,6 +6,7 @@ namespace Hello
         public static void Run()
         {
             Person person1 = new Person("Alice", 30);
+            person1.Age = 31; // Update age
             Person person2 = new Person("Bob", 25);
             Console.WriteLine($"Person 1: {person1.Name}, Age: {person1.Age}");
             Console.WriteLine($"Person 2: {person2.Name}, Age: {person2.Age}");
