dd2bcc2bd5f8d2129a59baef7cc8154799d5f0d3
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-09-01 11:44:13 -0400

    learn: magic number example

---
+++ Hello/MagicNumbers.cs
@@ -0,0 +1,11 @@
+﻿namespace Hello
+{
+    internal class MagicNumbers
+    {
+        public static void Run()
+        {
+            int number = 42; // This is a magic number
+            Console.WriteLine($"The answer to life, the universe, and everything is: {number}");
+        }
+    }
+}
