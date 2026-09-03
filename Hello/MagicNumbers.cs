4735af7b436addef441099676707708005de5d9c
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-09-03 16:42:21 -0400

    learn: basic magic number

--- Hello/MagicNumbers.cs
+++ Hello/MagicNumbers.cs
@@ -6,6 +6,21 @@
         {
             int number = 42; // This is a magic number
             Console.WriteLine($"The answer to life, the universe, and everything is: {number}");
+
+            // Bad practice: using numbers without explanation
+            int age = 20; // This is another magic number
+            if (age >= 18)
+            {
+                Console.WriteLine("Adult");
+            }
+
+            //Good practice: using named constants instead of magic numbers
+            const int ADULT_AGE = 18;
+
+            if (age >= ADULT_AGE)
+            {
+                Console.WriteLine("Adult");
+            }
         }
     }
 }
